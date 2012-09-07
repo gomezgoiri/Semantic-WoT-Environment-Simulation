1f9bfffa832acf6d1434e35ce5ae2d59c7ffa7de
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-07 16:40:19 +0000

    changed the mongo database

--- src/netuse/database/__init__.py
+++ src/netuse/database/__init__.py
@@ -1,2 +1,2 @@
 from mongoengine import connect
-connect('general_strategies', host="localhost") #, host='192.168.1.35', port=12345)
+connect('network_usage', host="localhost") #, host='192.168.1.35', port=12345)
