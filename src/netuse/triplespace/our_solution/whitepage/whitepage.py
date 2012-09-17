af1c8224d7ae9c893cc1540008159bf69fa5c880
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-17 17:17:25 +0000

    Module and package where all the stuff of whitepages will be located.

---
+++ src/netuse/triplespace/our_solution/whitepage/whitepage.py
@@ -0,0 +1,13 @@
+from netuse.triplespace.our_solution.clue_management import ClueStore
+
+class Whitepage(object):
+    
+    def __init__(self):
+        self.clues = ClueStore()
+        
+    def get_query_candidates(self, template):
+        self.clues.get_query_candidates(template)
+    
+    def add_clue(self, expiry_time, node, clue):
+        self.clues.add_clue(expiry_time, node, clue)
+        
