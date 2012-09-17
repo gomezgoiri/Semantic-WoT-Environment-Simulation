7154890bc872e33b3328de357411d851908a3bad
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-17 18:06:34 +0000

    Created a module to manage when the next update request should be sent by a consumer.

---
+++ src/netuse/triplespace/our_solution/consumer/time_update.py
@@ -0,0 +1,32 @@
+
+import numpy
+
+class UpdateTimesManager(object):
+    LAST_UPDATES = 10
+    MAX_UPDATE_RATE = 600000
+    MIN_UPDATE_RATE = 60000
+    DEFAULT_UPDATE_TIME = MAX_UPDATE_RATE
+    
+    def __init__(self):
+        self.last_updates = []
+        self.observers = []
+
+    def add_updatetime(self, time):
+        if len(self.last_updates)<UpdateTimesManager.LAST_UPDATES:
+            self.last_updates.append(time)
+        else:
+            del self.last_updates[0]
+            self.last_updates.append(time)
+            
+    def get_updatetime(self):
+        if len(self.last_updates)<2:
+            return UpdateTimesManager.DEFAULT_UPDATE_TIME
+        else:
+            mean_diff = numpy.mean([self.last_updates[i]-self.last_updates[i-1] for i in range(1,len(self.last_updates))])
+            
+            if mean_diff>UpdateTimesManager.MAX_UPDATE_RATE:
+                return UpdateTimesManager.MAX_UPDATE_RATE
+            elif mean_diff<UpdateTimesManager.MIN_UPDATE_RATE:
+                return UpdateTimesManager.MIN_UPDATE_RATE
+            else:
+                return mean_diff
---
+++ test/triplespace/our_solution/consumer/test_time_update.py
@@ -0,0 +1,38 @@
+import unittest
+from netuse.triplespace.our_solution.consumer.time_update import UpdateTimesManager
+
+class UpdateTimesManagerTestCase(unittest.TestCase):
+
+    def test_add_updatetime(self):
+        rc = UpdateTimesManager()
+        
+        l = []
+        for i in range(1, 101, 10):
+            rc.add_updatetime(i)
+            l.append(i)
+            self.assertEquals(rc.last_updates, l)
+        
+        rc.add_updatetime(14)
+        self.assertEquals(rc.last_updates, [11, 21, 31, 41, 51, 61, 71, 81, 91, 14])
+        
+    
+    def test_get_updatetime(self):
+        rc = UpdateTimesManager()
+        self.assertEquals(rc.get_updatetime(), UpdateTimesManager.MAX_UPDATE_RATE)
+        
+        for i in range(0, UpdateTimesManager.MIN_UPDATE_RATE*10, UpdateTimesManager.MIN_UPDATE_RATE-10):
+            rc.add_updatetime(i)
+        self.assertEquals(rc.get_updatetime(), UpdateTimesManager.MIN_UPDATE_RATE)
+        
+        middle_rate = (RemoteConnector.MAX_UPDATE_RATE - UpdateTimesManager.MIN_UPDATE_RATE) / 2 + RemoteConnector.MIN_UPDATE_RATE
+        for i in range(0, middle_rate*10, middle_rate):
+            rc.add_updatetime(i)
+        self.assertEquals(rc.get_updatetime(), middle_rate)
+        
+        for i in range(0, UpdateTimesManager.MAX_UPDATE_RATE*10, UpdateTimesManager.MAX_UPDATE_RATE+10):
+            rc.add_updatetime(i)
+        self.assertEquals(rc.get_updatetime(), UpdateTimesManager.MAX_UPDATE_RATE)
+            
+
+if __name__ == '__main__':
+    unittest.main()
