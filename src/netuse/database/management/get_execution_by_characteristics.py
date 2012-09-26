d2bc0bfd8c2a6324c104071fe8c3fa1daa8397fd
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-26 08:10:17 +0000

    Script to search for a specific execution set.

---
+++ src/netuse/database/management/get_execution_by_characteristics.py
@@ -0,0 +1,35 @@
+'''
+Created on Jan 7, 2012
+
+@author: tulvur
+'''
+from bson.objectid import ObjectId
+from netuse.database.parametrization import Parametrization
+from netuse.database.execution import ExecutionSet
+
+def search_execution(execution_set, strategy, num_nodes, num_consumers):
+    for ex in execution_set.executions:
+        params = ex.parameters
+        if params.strategy==strategy and len(params.nodes)==num_nodes and params.numConsumers==num_consumers:
+            print ex.id
+
+if __name__ == '__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser(description='Manage data.')
+    parser.add_argument('-o','--oid', default=None, dest='oid',
+                help='Specify the object id of the execution set to be looked for into. Otherwise the last execution set should be selected.')
+    parser.add_argument('-s','--strategy', default=Parametrization.our_solution, dest='strategy',
+                help='The strategy the searched execution should have.')
+    parser.add_argument('-N','--num_nodes', default=100, dest='num_nodes',
+                help='The number of nodes the searched execution should have.')
+    parser.add_argument('-Nc','--num_consumers', default=100, dest='num_consumers',
+                help='The number of consumer nodes the searched execution should have.')
+    
+    args = parser.parse_args()
+    objID = args.oid
+    
+    if objID==None:
+        search_execution(ExecutionSet.objects.first(), args.strategy, args.num_nodes, args.num_consumers)
+    else:
+        search_execution(ExecutionSet.objects(id=ObjectId(objID)).first(), args.strategy, args.num_nodes, args.num_consumers)
