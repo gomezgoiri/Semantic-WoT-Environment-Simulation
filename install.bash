84c7ea7be0b6fcf18b0239a40a0bf1d5a9923b37
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-27 19:37:43 +0000

    Installation requirements explained.

--- install.bash
+++ install.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo aptitude install mongodb-server
+
+
 mkvirtualenv netuse
 workon netuse
 
@@ -18,4 +21,12 @@ pip install matplotlib
 pip install -e svn+https://dev.morelab.deusto.es/svn/aigomez/trunk/NetworkUsage/#egg=netuse
 
 
-patch installation-path/rdflib/syntax/parsers/n3p/n3meta.py ./patches/n3meta.py.diff
+patch ./.virtualenvs/netuse/lib/python2.7/site-packages/rdflib-2.4.2-py2.7-linux-x86_64.egg/rdflib/syntax/parsers/n3p/n3meta.py ./.virtualenvs/netuse/src/netuse/patches/n3meta.py.diff
+patch ./.virtualenvs/netuse/lib/python2.7/site-packages/FuXi/Syntax/InfixOWL.py ./.virtualenvs/netuse/src/netuse/patches/InfixOWL.py.patch
+
+
+mkdir dev
+
+svn co https://dev.morelab.deusto.es/svn/aigomez/trunk/dataset/ -N dev/dataset
+svn co https://dev.morelab.deusto.es/svn/aigomez/trunk/dataset/base_ontologies/ dev/dataset/base_ontologies
+svn co https://dev.morelab.deusto.es/svn/aigomez/trunk/dataset/data/ dev/dataset/data
