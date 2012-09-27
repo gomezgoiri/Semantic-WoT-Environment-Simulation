84271ad710d710132526ecf335f235a92d17f5ce
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-27 18:08:58 +0000

    Server side code execution to count elements in a mongo ListField.

--- src/netuse/database/pymongo_dao.py
+++ src/netuse/database/pymongo_dao.py
@@ -14,8 +14,23 @@ class PyMongoDAO(object):
         self.db = self.connection[database]
     
     def count_traces_in_execution(self, objectid):
-        ex = self.db.executions.find_one({"_id": objectid}) # or directly find_one(objectid)        
-        return len(ex['requests'])
+        #ex = self.db.executions.find_one({"_id": objectid}) # or directly find_one(objectid)
+        
+        # Pablo's original mongodb command:
+        #db.eval(function(){return db.executions.find().map(function (obj) { return obj.requests.length });})
+        server_func = """
+                            function() {
+                                return db.executions.find({'_id': ObjectId('%s')}).map(
+                                    function (obj) {
+                                        return obj.requests.length;
+                                    }
+                                );
+                            }
+                        """%(objectid)
+                        
+        #print server_func.replace("\n", "").replace("\t", "").replace("  ", "")
+        return self.db.eval(server_func)
+        # return len(ex['requests']) # easy way: it loads locally all the elements (AVOIDED!)
     
     def get_simulated_execution_sets(self):
         esets = self.db.executionSet.find()
@@ -24,7 +39,7 @@ class PyMongoDAO(object):
         for es in esets:
             all_simulated = True
             for ex_dbref in es["executions"]:
-                ex = self.db.executions.find_one(ex_dbref.id)
+                ex = self.db.executions.find_one(ex_dbref.id, {'requests':0} )
                 if "execution_date" not in ex:
                     all_simulated = False
                     break
@@ -41,9 +56,13 @@ class PyMongoDAO(object):
         
         for es in esets:
             for ex_dbref in es["executions"]:
-                ex = self.db.executions.find_one(ex_dbref.id)
+                ex = self.db.executions.find_one(ex_dbref.id, {'requests':0})
                 if "execution_date" not in ex:
                     unsimulated.append(es["_id"])
                     break
                 
         return unsimulated
+    
+    def get_execution_ids(self, executionSet_id):
+        es = self.db.executionSet.find_one({"_id": executionSet_id})                
+        return [ex_dbref.id for ex_dbref in es["executions"]]
