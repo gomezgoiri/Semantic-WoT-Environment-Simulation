a55f76afaed8c6906190d1f350a590c3b956643e
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-09-28 09:07:44 +0000

    More simulation execution modes were implemented (to allow multiprocessing in each machine running a simulation).

--- src/netuse/evaluation/simulate.py
+++ src/netuse/evaluation/simulate.py
@@ -16,11 +16,10 @@ from FuXi.DLP.DLNormalization import NormalFormReduction
 
 from netuse.activity import ActivityGenerator
 from netuse.nodes import NodeGenerator
-from netuse.database.execution import ExecutionSet
+from netuse.database.execution import ExecutionSet, Execution
 from netuse.results import G
 
-from multiprocessing import Process
-
+from multiprocessing import Process, Queue, cpu_count
 
 
 def performSimulation(execution, preloadedGraph={}):
@@ -72,43 +71,58 @@ def loadGraphsJustOnce(nodeNames, semanticPath, loadedGraph):
                     if fname.find(node_name)!=-1:
                         loadedGraph[node_name].append( Graph().parse(datasetPath+"/"+fname, format="n3") )
 
-
-def mark_execution(execution):
-    ''' This method is used to warn another processes that this one is already processing it.'''
-    execution.execution_date = datetime.datetime.now()
-    execution.save()
-
-def execute_all_concurrently(executions):
-    processes = []
-    for ex in executions:
-        if ex.execution_date!=None and ex.parameters!=None:
-            mark_execution(ex)
-            p = Process(target=performSimulation, args=(ex,))
-            p.start()
-            processes.append(p)
-    for p in processes:
-        p.join()
+            
+class SimulationPerformer(Process):
     
-def execute_once_each_time(executions):
-    # loadedGraphs = {}
-    for ex in executions:
-        # the execution_date may have been changed by another process in the meantime...
-        ex.reload() # ensure that we have an updated value of ex.execution_date
-        if ex.execution_date==None and ex.parameters!=None:
-            mark_execution(ex)
-            # In a new process to ensure that the memory is freed after that
-            p = Process(target=performSimulation, args=(ex,))
-            p.start()
-            p.join()
-            # Otherwise, to avoid loading graphs each time...
-            #performSimulation(ex, loadedGraphs)
+    def __init__(self, name=None, queue=None):
+        Process.__init__(self, name=name)
+        self.execution_ids = queue
+    
+    def mark_execution(self, execution):
+        ''' This method is used to warn another processes that this one is already processing it.'''
+        execution.execution_date = datetime.datetime.now()
+        execution.save()
+    
+    def run(self):
+        while not self.execution_ids.empty():
+            execution_id = self.execution_ids.get() # race condition between while and this sentence
+            ex = Execution.objects(id=execution_id).first()
+            if ex.execution_date==None and ex.parameters!=None: # race condition in the mongodb update between processes in different machines
+                self.mark_execution(ex) # 2 processes in different processes (or machines)
+                performSimulation(ex)
+
+def execute_in_n_processors(executionIds_queue, num_processes):
+    processes = []
+    try:
+        for n in range(num_processes):
+            sp = SimulationPerformer("Process_%d"%(n), executionIds_queue)
+            processes.append(sp)
+            sp.start()
+    finally:
+        for process in processes:
+            process.join()
+
+def execute_once_each_time(executionIds_queue):
+    execute_in_n_processors(executionIds_queue, 1)
+
+def execute_all_concurrently(executionIds_queue):
+    execute_in_n_processors(executionIds_queue, executionIds_queue.qsize())
+    
+def execute_one_in_each_processor(executionIds_queue):
+    execute_in_n_processors(executionIds_queue, cpu_count())
+
 
 def simulateUnsimulatedExecutionSet():
     one_es_per_execution = True # just one simulation (ExecutionSet) per execution
     
     for es in ExecutionSet.objects.get_unsimulated():
-        #execute_all_concurrently(es.executions)
-        execute_once_each_time(es.executions)
+        executionIds_queue = Queue()
+        for ex in es.executions:
+            executionIds_queue.put(ex.id)
+        
+        #execute_all_concurrently(executionIds_queue)
+        execute_one_in_each_processor(executionIds_queue)
+        #execute_once_each_time(executionIds_queue)
         
         if one_es_per_execution:
             break
