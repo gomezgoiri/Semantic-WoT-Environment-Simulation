182fd155cd12471971cad5825debb86b596d4305
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-10-26 07:54:53 +0000

    Using variables for virtualenv installation path and environment name.

--- install.bash
+++ install.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkvirtualenv netuse
-workon netuse
+export env_name=netuse
+export virtualenv_path=~/.virtualenvs
+
+
+mkvirtualenv $env_name
+workon $env_name
 
 
 pip install simpy
@@ -18,8 +22,8 @@ pip install matplotlib
 pip install -e svn+https://dev.morelab.deusto.es/svn/aigomez/trunk/NetworkUsage/#egg=netuse
 
 
-patch ./.virtualenvs/netuse/lib/python2.7/site-packages/rdflib-2.4.2-py2.7-linux-x86_64.egg/rdflib/syntax/parsers/n3p/n3meta.py ./.virtualenvs/netuse/src/netuse/patches/n3meta.py.diff
-patch ./.virtualenvs/netuse/lib/python2.7/site-packages/FuXi/Syntax/InfixOWL.py ./.virtualenvs/netuse/src/netuse/patches/InfixOWL.py.patch
+patch $virtualenv_path/$env_name/lib/python2.7/site-packages/rdflib-2.4.2-py2.7-linux-x86_64.egg/rdflib/syntax/parsers/n3p/n3meta.py $virtualenv_path/$env_name/src/netuse/patches/n3meta.py.diff
+patch $virtualenv_path/$env_name/lib/python2.7/site-packages/FuXi/Syntax/InfixOWL.py $virtualenv_path/$env_name/src/netuse/patches/InfixOWL.py.patch
 
 
 mkdir dev
