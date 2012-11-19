cdb2891bf53abb443d04743efc1e3659ac198969
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-11-19 10:02:24 +0100

    Forcing flush depending on the time passed since the last one or the amount of writes.

--- src/netuse/tracers.py
+++ src/netuse/tracers.py
@@ -4,6 +4,7 @@ Created on Sep 26, 2012
 @author: tulvur
 '''
 
+from time import time
 from abc import ABCMeta, abstractmethod
 
 
@@ -22,11 +23,34 @@ class AbstractTracer(object):
     def trace(self, timestamp, client, server, path, status, response_time):
         pass
 
+# http://stackoverflow.com/questions/3167494/how-often-does-python-flush-to-a-file
+class Flusher(object):
+    '''
+        To force flush to a file depending on the number of writes or the time since the last flush.
+        
+        Using this class, we can check the simulation output in real-time.
+    '''
+    
+    WRITES = 10 # write before flushing
+    TIME = 1000 # time before last flush
+    
+    def __init__(self):
+        self.countdown = Flusher.WRITES
+        self.last_flush = 0
+    
+    def force_flush(self):
+        t = time()
+        if self.countdown<0 or t - self.last_flush<Flusher.TIME:
+            self.countdown = Flusher.WRITES
+            self.last_flush = t
+            return True # you need to flush
+        return False # don't need to force the flush
 
 class FileTracer(AbstractTracer):
     
     def __init__(self, filename='/tmp/workfile'):
         self.filename = filename
+        self.flusher = Flusher(self.filename)
     
     def start(self):
         self.f = open(self.filename, 'w')
@@ -36,6 +60,9 @@ class FileTracer(AbstractTracer):
     
     def trace(self, timestamp, client, server, path, status, response_time):
         self.f.write("%0.2f\t%0.2f\t%s\t%s\t%s\t%d\n"%(timestamp,response_time,client,server,path,status))
+        
+        if self.flusher.force_flush():
+            self.f.flush()
 
 
 class MongoDBTracer(AbstractTracer):
