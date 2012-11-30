159e6573ed384edb812ecc4b5dce6498926f7383
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2012-11-30 18:43:05 +0100

    Simulation module objectified (issue:3)

--- src/netuse/evaluation/simulate.py
+++ src/netuse/evaluation/simulate.py
@@ -6,7 +6,7 @@ Created on Nov 26, 2011
 import datetime
 
 # "SimulationTrace" instead of "Simulation" to debug
-from SimPy.Simulation import initialize, simulate
+from SimPy.Simulation import Simulation
 from netuse.activity import ActivityGenerator
 from netuse.nodes import NodeGenerator
 from netuse.database.execution import ExecutionSet, Execution
@@ -16,46 +16,53 @@ from commons.utils import SemanticFilesLoader
 from multiprocessing import Process, Queue, cpu_count
 
 
-def performSimulation(execution, preloadedGraph={}):
-    print "New simulation: %s"%(execution.parameters)
+class Model(Simulation):
+    def __init__(self, execution):
+        super(Model, self).__init__()
+        self.execution = execution
     
-    sfl = SemanticFilesLoader(G.dataset_path)
-    sfl.loadGraphsJustOnce(execution.parameters.nodes, preloadedGraph)
+    def mark_execution(self):
+        ''' This method is used to warn another processes that this one is already processing it.'''
+        self.execution.execution_date = datetime.datetime.now()
+        self.execution.save()
     
-    initialize()
-    G.setNewExecution(execution)
-    
-    nodes = NodeGenerator(execution.parameters)
-    nodes.generateNodes()
-    
-    activity = ActivityGenerator(execution.parameters, preloadedGraph)
-    activity.generateActivity()
-    
-    # activate
-    cool_down = 500
-    simulate(until=execution.parameters.simulateUntil+cool_down)
-    
-    G.shutdown()
+    def runModel(self):
+        self.mark_execution() # 2 processes in different processes (or machines)
+        
+        print "New simulation: %s"%(self.execution.parameters)
+        
+        sfl = SemanticFilesLoader(G.dataset_path)
+        sfl.loadGraphsJustOnce(self.execution.parameters.nodes, preloadedGraph={})
+        
+        self.initialize()
+        G.setNewExecution(self.execution)
+        
+        nodes = NodeGenerator(self.execution.parameters)
+        nodes.generateNodes()
+        
+        activity = ActivityGenerator(self.execution.parameters, preloadedGraph={})
+        activity.generateActivity()
+        
+        # activate
+        cool_down = 500
+        self.simulate( until = self.execution.parameters.simulateUntil + cool_down )
+        
+        G.shutdown()
 
-            
+
 class SimulationPerformer(Process):
     
     def __init__(self, name=None, queue=None):
         super(SimulationPerformer, self).__init__(name=name)
         self.execution_ids = queue
     
-    def mark_execution(self, execution):
-        ''' This method is used to warn another processes that this one is already processing it.'''
-        execution.execution_date = datetime.datetime.now()
-        execution.save()
-    
     def run(self):
         while not self.execution_ids.empty():
             execution_id = self.execution_ids.get() # race condition between while and this sentence
-            ex = Execution.objects(id=execution_id).first()
+            ex = Execution.objects( id=execution_id ).first()
             if ex.execution_date==None and ex.parameters!=None: # race condition in the mongodb update between processes in different machines
-                self.mark_execution(ex) # 2 processes in different processes (or machines)
-                performSimulation(ex)
+                model = Model(ex)
+                model.runModel()
 
 def execute_in_n_processors(executionIds_queue, num_processes):
     processes = []
