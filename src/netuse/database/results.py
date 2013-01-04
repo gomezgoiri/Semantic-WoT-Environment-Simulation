377df4b2d821ceac1f19dcf1fe1c17079f58dacc
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-01-04 11:16:30 +0100

    Removed outdated result objects

--- src/netuse/database/results.py
+++ src/netuse/database/results.py
@@ -21,31 +21,3 @@ class NetworkTrace(Document):
     
     # Possible extensions
     #  + bandwidth (or data exchanged)
-    #  + path = StringField()
-    
-    
-
-class RequestsResults(Document):
-    meta = {'collection': 'results'}
-    
-    found = IntField(default=0)
-    not_found = IntField(default=0)
-    server_error = IntField(default=0)
-    timeout = IntField(default=0)
-    success = IntField(default=0)
-    failure = IntField(default=0)
-    total = IntField(default=0)
-
-class Results(Document):
-    meta = {'collection': 'results'}
-    
-    requests = ReferenceField(RequestsResults)
-    response_time_mean = FloatField(default=0.0)
-    data_exchanged = FloatField(default=0.0)
-
-
-# A medir:
-#    numero de comunicaciones necesarias / producidas (eficiencia)
-#    tiempo medio en el que cada nodo ha estado ocupado
-#    tiempo necesario para obtener respuesta
-#    cuando sporadic pasa a ser continuous? (dependiendo de las var indep)
