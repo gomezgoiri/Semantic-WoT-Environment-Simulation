7321eb7df957227c9c810d98fe9bad5a1f3c060a
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-01-12 21:46:40 +0100

    Adding new events on record registration

--- src/netuse/mdns/cache.py
+++ src/netuse/mdns/cache.py
@@ -4,33 +4,55 @@ Created on Jan 12, 2013
 @author: tulvur
 '''
 
-class Cache(object):
+from random import Random
+from SimPy.Simulation import Process
+
+class Cache(Process):
     
     WHEN_FIELD = 0
     ACTION_FIELD = 1 # what to do
-    RECORD = 2
+    RECORD_FIELD = 2
     
-    def __init__(self):
+    EVENT_USE_MULTICAST = "response_by_multicast"
+    EVENT_NOT_KNOWN_ANSWER = "unadded_known_answer_suppression"
+    EVENT_RENEW = "try_to_renew"
+    
+    def __init__(self, sim):
+        super(Cache, self).__init__(sim=sim)
         self.pending_events = [] # tuples with the form (when, action, record)
         self.records = [] # cached records
+        self._random = Random()
     
     def _delete_events_for_record(self, record):
         to_delete = []
         for event in self.pending_events:
-            if event[Cache.RECORD] == record:
+            if event[Cache.RECORD_FIELD] == record:
                 to_delete.append(event)
         
         for event in to_delete:
             self.pending_events.remove(event)
     
+    def _get_time_after_percentage(self, ttl, percentage):
+        """Percentage example: 0.45 (means 45%)"""
+        return self.sim.now() + ttl * percentage
+    
     def _create_new_events(self, record):
-        to_delete = []
-        for event in self.pending_events:
-            if event[Cache.RECORD] == record:
-                to_delete.append(event)
+        ttl = record.ttl
         
-        for event in to_delete:
-            self.pending_events.remove(event)
+        # at 1/4 of the TTL => no more QU responses
+        when = self._get_time_after_percentage(ttl, 0.25)
+        self.pending_events.append( (when, Cache.EVENT_USE_MULTICAST, record) )
+        
+        # at 1/2 of the TTL => does not add to known answer suppression
+        when = self._get_time_after_percentage(ttl, 0.5)
+        self.pending_events.append( (when, Cache.EVENT_NOT_KNOWN_ANSWER, record) )
+        
+        # section 5.2, http://tools.ietf.org/html/draft-cheshire-dnsext-multicastdns-15
+        # at 80%, 85%, 90% and 95% of the TTL => try to renew the record
+        for percentage in ( 80, 85, 90, 95 ):
+            percentage_with_variation = (percentage + self._random.random()*2) / 100.0 # 2% of variation
+            when = self._get_time_after_percentage(ttl, percentage_with_variation)
+            self.pending_events.append( (when, Cache.EVENT_RENEW, record) )
     
     def cache_record(self, record):
         self._delete_events_for_record(record)
@@ -39,5 +61,5 @@ class Cache(object):
         self.records.remove(record) # does delete the previous one?
         self.records.append(record)
     
-    def maintain(self):
+    def wait_for_next_event(self):
         pass
--- test/netuse/mdns/test_cache.py
+++ test/netuse/mdns/test_cache.py
@@ -5,39 +5,66 @@ Created on Nov 28, 2011
 '''
 
 import unittest
+from mock import Mock, patch
 from netuse.mdns.cache import Cache
 from netuse.mdns.record import PTRRecord, SVRRecord, TXTRecord
 
 class TestCache(unittest.TestCase):
     
     def setUp(self):
-        self.cache = Cache()
+        self.cache = Cache(None)
         for i in range(5):
             at = i * 10
             self.cache.pending_events.append((at, "blah", PTRRecord("name%d._http._tcp.local"%i)))
             self.cache.pending_events.append((at, "blah", SVRRecord("name%d._http._tcp.local"%i, None, None)))
             self.cache.pending_events.append((at, "blah", TXTRecord("name%d._http._tcp.local"%i, {})))
         self.assertEquals(15, len(self.cache.pending_events))
+        
+        self.sample_txt = TXTRecord("name0._http._tcp.local", {})
+        self.sample_svr = SVRRecord("name2._http._tcp.local", None, None)
+        self.sample_ptr = PTRRecord("name4._http._tcp.local")
     
-    def assert_not_event(self, ttype, name):
+    def does_contains_event(self, ttype, name, when=None, action=None):
         for event in self.cache.pending_events:
-            self.assertFalse( event[Cache.RECORD].name == name and event[Cache.RECORD].type == ttype)
+            cond = event is None or event[Cache.WHEN_FIELD]==when
+            cond = cond and ( event is None or event[Cache.ACTION_FIELD]==action )
+            cond = cond and event[Cache.RECORD_FIELD].name == name
+            cond = cond and event[Cache.RECORD_FIELD].type == ttype
+            if cond:
+                return True
+        return False
     
     def test_delete_events_for_record(self):
-        record_to_delete = TXTRecord("name0._http._tcp.local", {})
-        self.cache._delete_events_for_record(record_to_delete)
+        self.cache._delete_events_for_record(self.sample_txt)
         self.assertEquals(14, len(self.cache.pending_events))
-        self.assert_not_event(record_to_delete.type, record_to_delete.name)
+        self.assertFalse( self.does_contains_event(self.sample_txt.type, self.sample_txt.name) )
         
-        record_to_delete = SVRRecord("name2._http._tcp.local", None, None)
-        self.cache._delete_events_for_record(record_to_delete)
+        self.cache._delete_events_for_record(self.sample_svr)
         self.assertEquals(13, len(self.cache.pending_events))
-        self.assert_not_event(record_to_delete.type, record_to_delete.name)
+        self.assertFalse( self.does_contains_event(self.sample_svr.type, self.sample_svr.name) )
         
-        record_to_delete = PTRRecord("name4._http._tcp.local")
-        self.cache._delete_events_for_record(record_to_delete)
+        self.cache._delete_events_for_record(self.sample_ptr)
         self.assertEquals(12, len(self.cache.pending_events))
-        self.assert_not_event(record_to_delete.type, record_to_delete.name)
+        self.assertFalse( self.does_contains_event(self.sample_ptr.type, self.sample_ptr.name) )
+
+    def test_create_new_events(self):
+        self.cache._random = Mock()
+        self.cache._random.random.side_effect = lambda *args: 0.5 # 0.5 * 2% == 1% of variation
+        self.cache.sim = Mock()
+        self.cache.sim.now.side_effect = lambda: 0
+        
+        self.sample_txt.ttl = 1000 # to ease calculations
+        self.cache._create_new_events(self.sample_txt)
+        self.assertEquals(15+6, len(self.cache.pending_events))
+        
+        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=250, action=Cache.EVENT_USE_MULTICAST) )
+        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=500, action=Cache.EVENT_NOT_KNOWN_ANSWER) )
+        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=810, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=860, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=910, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=960, action=Cache.EVENT_RENEW) )
+        
+
 
 if __name__ == '__main__':    
     unittest.main()
