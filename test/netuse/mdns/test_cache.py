4a8e52b404262bd2a8f8f3b756153e2c8cacf6d2
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-01-13 21:30:21 +0100

    Fixing last bug related with TTLs measured in seconds

--- test/netuse/mdns/test_cache.py
+++ test/netuse/mdns/test_cache.py
@@ -39,7 +39,7 @@ class TestCache(unittest.TestCase):
         self.sample_svr = SVRRecord("name2._http._tcp.local", None, None)
         self.sample_ptr = PTRRecord("_http._tcp.local", "name4._http._tcp.local")
     
-    def does_contains_event(self, ttype, name, when=None, action=None):
+    def does_contain_event(self, ttype, name, when=None, action=None):
         for event in self.cache.pending_events:
             cond = event is None or event[Cache.WHEN_FIELD]==when
             cond = cond and ( event is None or event[Cache.ACTION_FIELD]==action )
@@ -52,15 +52,15 @@ class TestCache(unittest.TestCase):
     def test_delete_events_for_record(self):
         self.cache._delete_events_for_record(self.sample_txt)
         self.assertEquals(14, len(self.cache.pending_events))
-        self.assertFalse( self.does_contains_event(self.sample_txt.type, self.sample_txt.name) )
+        self.assertFalse( self.does_contain_event(self.sample_txt.type, self.sample_txt.name) )
         
         self.cache._delete_events_for_record(self.sample_svr)
         self.assertEquals(13, len(self.cache.pending_events))
-        self.assertFalse( self.does_contains_event(self.sample_svr.type, self.sample_svr.name) )
+        self.assertFalse( self.does_contain_event(self.sample_svr.type, self.sample_svr.name) )
         
         self.cache._delete_events_for_record(self.sample_ptr)
         self.assertEquals(12, len(self.cache.pending_events))
-        self.assertFalse( self.does_contains_event(self.sample_ptr.type, self.sample_ptr.name) )
+        self.assertFalse( self.does_contain_event(self.sample_ptr.type, self.sample_ptr.name) )
 
     def test_create_new_events(self):
         self.cache._random = Mock()
@@ -68,15 +68,15 @@ class TestCache(unittest.TestCase):
         self.cache.sim = Mock()
         self.cache.sim.now.side_effect = lambda: 0
         
-        self.sample_txt.ttl = 1000 # to ease calculations
+        self.sample_txt.ttl = 1 # to ease calculations
         self.cache._create_new_events(self.sample_txt)
         self.assertEquals(15+5, len(self.cache.pending_events))
         
-        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=500, action=Cache.EVENT_NOT_KNOWN_ANSWER) )
-        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=810, action=Cache.EVENT_RENEW) )
-        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=860, action=Cache.EVENT_RENEW) )
-        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=910, action=Cache.EVENT_RENEW) )
-        self.assertTrue( self.does_contains_event(self.sample_txt.type, self.sample_txt.name, when=960, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contain_event(self.sample_txt.type, self.sample_txt.name, when=500, action=Cache.EVENT_NOT_KNOWN_ANSWER) )
+        self.assertTrue( self.does_contain_event(self.sample_txt.type, self.sample_txt.name, when=810, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contain_event(self.sample_txt.type, self.sample_txt.name, when=860, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contain_event(self.sample_txt.type, self.sample_txt.name, when=910, action=Cache.EVENT_RENEW) )
+        self.assertTrue( self.does_contain_event(self.sample_txt.type, self.sample_txt.name, when=960, action=Cache.EVENT_RENEW) )
         
     def test_cache_record(self):
         new_record = copy.deepcopy(self.sample_txt)
