839d947c3d51d8df749d9e48b560a283f8fd79be
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-01-22 18:58:51 +0100

    Added methods related with the first load in the WP

--- src/netuse/triplespace/our_solution/whitepage/whitepage.py
+++ src/netuse/triplespace/our_solution/whitepage/whitepage.py
@@ -2,12 +2,10 @@ from clueseval.clues.storage.sqlite import SQLiteClueStore
 
 class Whitepage(object):
     
-    def __init__(self, clue_store, generation_time = None):
-        if clue_store is None:
-            self.clues = SQLiteClueStore(generation_time, in_memory=True) # generation time == 0, this should change
-            self.clues.start() # TODO when should be stopped???
-        else:
-            self.clues = clue_store
+    def __init__(self, generation_time = None):
+        self.clues = SQLiteClueStore(generation_time, in_memory=True) # generation time == 0, this should change
+        self.loaded_version = self.clues.version
+        self.clues.start() # TODO when should be stop it???
         
     def get_query_candidates(self, template):
         return self.clues.get_query_candidates(template)
@@ -18,3 +16,12 @@ class Whitepage(object):
         
     def get_aggregated_clues_json(self):
         return self.clues.toJson()
+    
+    def initial_data_loading(self, aggregated_clues_json):
+        actual_version = self.clues.version # since the version changes on fromJson()
+        self.clues.fromJson( aggregated_clues_json )
+        self.loaded_version = self.clues.version
+        self.clues.set_version( actual_version.generation, actual_version.version )
+    
+    def get_initially_loaded_version(self):
+        return self.loaded_version
