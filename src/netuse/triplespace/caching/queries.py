7552e7c6a1b4e7f1cc32af8b2fe09f8bd8c6f795
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-24 00:26:52 +0200

    Cache implementation now takes into account template queries

--- src/netuse/triplespace/caching/queries.py
+++ src/netuse/triplespace/caching/queries.py
@@ -7,34 +7,45 @@ Created on May 23, 2013
 class QueryCacher(object):
 
     def __init__(self):
-        self.positive_answers = set()
-        self.negative_answers = set()
+        self.positive_answers = {} # dict of set()
+        self.negative_answers = {} # dict of set()
     
-    def get_relevant_nodes(self, all_nodes):
+    def get_relevant_nodes(self, template, all_nodes):
         '''Returns the following nodes:
              * Nodes which have answered to this query in the past
              * Nodes that have not answered to this query in the past
         '''
         relevant = set()
+        if template not in self.negative_answers:
+            return set(all_nodes) # no matter if they are positive or unknown
+        
         for node in all_nodes:
             node_name = str(node)
-            if node_name in self.positive_answers:
+            if template in self.positive_answers and node_name in self.positive_answers[template]:
                 relevant.add(node)
-            elif node_name not in self.negative_answers:
+            elif template in self.negative_answers and node_name not in self.negative_answers[template]:
                 relevant.add(node)
         return relevant 
     
-    def cache(self, node, response):
+    def cache(self, template, node, response):
         node_name = str(node)
         
         if response.getstatus() == 200:
-            if node_name in self.negative_answers: # not possible, but anyway...
-                self.negative_answers.remove(node_name)
-            self.positive_answers.add(node_name)
+            if template in self.negative_answers:
+                if node_name in self.negative_answers[template]: # not possible, but anyway...
+                    self.negative_answers[template].remove(node_name)
+            
+            if template not in self.positive_answers:
+                self.positive_answers[template] = set()
+            self.positive_answers[template].add(node_name)
              
         elif response.getstatus() == 404:
-            if node_name in self.positive_answers:
-                self.positive_answers.remove(node_name)
-            self.negative_answers.add(node_name)
+            if template in self.positive_answers:
+                if node_name in self.positive_answers[template]:
+                    self.positive_answers[template].remove(node_name)
+                
+            if template not in self.negative_answers:
+                self.negative_answers[template] = set()
+            self.negative_answers[template].add(node_name)
              
         # other status are ignored: the previous state of the node in the cache is maintained
--- test/netuse/triplespace/caching/test_queries.py
+++ test/netuse/triplespace/caching/test_queries.py
@@ -5,6 +5,7 @@ Created on May 23, 2013
 '''
 
 import unittest
+from rdflib import Namespace
 from netuse.triplespace.caching.queries import QueryCacher
 from netuse.triplespace.network.httpelements import HttpResponse
 
@@ -22,12 +23,20 @@ class FakeNode(object):
         return self.name
 
 
+ns_test = Namespace("http://www.morelab.deusto.es/test#")
+
 class TestQueryCacher(unittest.TestCase): # classes under test: DelayedRequest, ScheduledRequest
 
     def setUp(self):
+        self.template1 = ( ns_test.subj1, ns_test.pred1, ns_test.obj1)
+        self.template2 = ( ns_test.subj2, None, None)
+        self.template3 = ( ns_test.subj3, ns_test.pred1, ns_test.obj1)
+        
         self.cache = QueryCacher()
-        self.cache.positive_answers = set(["node1", "node2"])
-        self.cache.negative_answers = set(["node3", "node4", "node5"])
+        self.cache.positive_answers = { self.template1: set(["node1", "node2"]),
+                                        self.template2: set(["node1_t2", "node2_t2"]) }
+        self.cache.negative_answers = { self.template1: set(["node3", "node4", "node5"]),
+                                        self.template2: set(["node3_t2", "node4_t2", "node5_t2"]) }
     
     def test_get_relevant_nodes(self):
         nodes = [ FakeNode(8, "node8"), # unknown
@@ -35,34 +44,56 @@ class TestQueryCacher(unittest.TestCase): # classes under test: DelayedRequest, 
                   FakeNode(9, "node9"), # unknown
                   FakeNode(3, "node3") ] # negative response in the past
         
-        relevant = self.cache.get_relevant_nodes( nodes )
+        relevant = self.cache.get_relevant_nodes( self.template1, nodes )
         self.assertEquals( set([ nodes[0], nodes[1], nodes[2] ]), relevant )
+        
+        relevant = self.cache.get_relevant_nodes( self.template2, nodes )
+        self.assertEquals( set(nodes), relevant )
 
     def test_cache_positive(self):
-        self.cache.cache( FakeNode(10, "node10"), HttpResponse(3, "blah", status=200) )
-        self.assertEquals( 3, len(self.cache.positive_answers) )
-        self.assertEquals( 3, len(self.cache.negative_answers) )
-        self.assertTrue( "node10" in self.cache.positive_answers )
+        self.cache.cache( self.template1,
+                          FakeNode(10, "node10"),
+                          HttpResponse(3, "blah", status=200) )
+        self.assertEquals( 3, len(self.cache.positive_answers[self.template1]) )
+        self.assertEquals( 3, len(self.cache.negative_answers[self.template1]) )
+        self.assertTrue( "node10" in self.cache.positive_answers[self.template1] )
+        self.assertEquals( 2, len(self.cache.positive_answers[self.template2]) )
         
     def test_cache_negative(self):
-        self.cache.cache( FakeNode(11, "node11"), HttpResponse(3, "blah", status=404) )
-        self.assertEquals( 2, len(self.cache.positive_answers) )
-        self.assertEquals( 4, len(self.cache.negative_answers) )
-        self.assertTrue( "node11" in self.cache.negative_answers )
+        self.cache.cache( self.template1,
+                          FakeNode(11, "node11"),
+                          HttpResponse(3, "blah", status=404) )
+        
+        self.assertEquals( 2, len(self.cache.positive_answers[self.template1]) )        
+        self.assertEquals( 4, len(self.cache.negative_answers[self.template1]) )
+        self.assertTrue( "node11" in self.cache.negative_answers[self.template1] )
+        self.assertEquals( 3, len(self.cache.negative_answers[self.template2]) )  
         
     def test_cache_negative_former_positive(self):
-        self.cache.cache( FakeNode(2, "node2"), HttpResponse(3, "blah", status=404) )
-        self.assertEquals( 1, len(self.cache.positive_answers) )
-        self.assertFalse( "node2" in self.cache.positive_answers )
-        self.assertEquals( 4, len(self.cache.negative_answers) )
-        self.assertTrue( "node2" in self.cache.negative_answers )
+        self.cache.cache( self.template1,
+                          FakeNode(2, "node2"),
+                          HttpResponse(3, "blah", status=404) )
+        
+        self.assertEquals( 1, len(self.cache.positive_answers[self.template1]) )
+        self.assertFalse( "node2" in self.cache.positive_answers[self.template1] )
+        self.assertEquals( 2, len(self.cache.positive_answers[self.template2]) )
+        
+        self.assertEquals( 4, len(self.cache.negative_answers[self.template1]) )
+        self.assertTrue( "node2" in self.cache.negative_answers[self.template1] )
+        self.assertEquals( 3, len(self.cache.negative_answers[self.template2]) )
     
     def test_cache_positive_former_negative(self):
-        self.cache.cache( FakeNode(5, "node5"), HttpResponse(3, "blah", status=200) )
-        self.assertEquals( 3, len(self.cache.positive_answers) )
-        self.assertTrue( "node5" in self.cache.positive_answers )
-        self.assertEquals( 2, len(self.cache.negative_answers) )
-        self.assertFalse( "node5" in self.cache.negative_answers )
+        self.cache.cache( self.template1,
+                          FakeNode(5, "node5"),
+                          HttpResponse(3, "blah", status=200) )
+        
+        self.assertEquals( 3, len(self.cache.positive_answers[self.template1]) )
+        self.assertTrue( "node5" in self.cache.positive_answers[self.template1] )
+        self.assertEquals( 2, len(self.cache.positive_answers[self.template2]) )
+        
+        self.assertEquals( 2, len(self.cache.negative_answers[self.template1]) )
+        self.assertFalse( "node5" in self.cache.negative_answers[self.template1] )
+        self.assertEquals( 3, len(self.cache.negative_answers[self.template2]) )
 
 if __name__ == '__main__':
     unittest.main()
